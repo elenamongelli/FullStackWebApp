6540e05b4ad28da43e9d4b756312d201e101572c
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2023-02-01 16:26:27 +0100

    Create APIview

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from django.shortcuts import render
+from .serializers import RoomSerializer
+from .models import Room
 
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
